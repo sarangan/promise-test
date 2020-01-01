713d56faf6a93fd33f86d150cb01a17202cd1e58
sara <sarangan12@gmail.com>
2020-01-01 11:46:23 +0800

    Update index.js

--- index.js
+++ index.js
@@ -4,16 +4,16 @@ function rp(){
     });
 }
   
-async function a(){
-    const r = await rp();
+function a(){
+    const r = rp();
     console.log('a - ' , r);
-    return r + 1;
+    return r;
 }
   
-async function b(){
-    const r = await rp();
+function b(){
+    const r = rp();
     console.log('b - ' , r);
-    return r + 2;
+    return r;
 }
   
 async function main(){
@@ -21,15 +21,26 @@ async function main(){
     // const ax = await a();
     // const bx = await b();
 
-    const [ax, bx] = await Promise.race([a(),b()]);
+    const [ax, bx] = await Promise.all([a(),b()]);
     console.log(ax);
     console.log(bx);
-
-    const s = await a();
-    console.log(s);
 
   }
   
   main();
 
-  
+
+async function race(){
+    var promise1 = new Promise(function(resolve, reject) {
+        setTimeout(resolve, 500, 'one');
+    });
+    
+    var promise2 = new Promise(function(resolve, reject) {
+        setTimeout(resolve, 100, 'two');
+    });
+
+    const p = await Promise.race([promise1, promise2]);
+    console.log(p);
+}
+
+race();
